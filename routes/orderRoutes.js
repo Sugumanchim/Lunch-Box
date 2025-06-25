d8c42285f6a344bd7ce650ae06243d8d3260aaac
Sugumanchim <sugumanchim@gmail.com>
2025-06-25 22:38:48 +0530

    orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { placeOrder, getOrders } = require('../controllers/orderController');
+
+const {
+  placeOrder,
+  getAllOrders,
+  getOrderById,
+  updateOrder,
+  deleteOrder,
+  getOrdersByUserId
+} = require('../controllers/orderController');
+
+// Route definitions
 router.post('/', placeOrder);
-router.get('/:userId', getOrders);
+router.get('/', getAllOrders);
+router.get('/:id', getOrderById); // for individual order
+router.get('/user/:userId', getOrdersByUserId); // optional: fetch by user
+router.put('/:id', updateOrder);
+router.delete('/:id', deleteOrder);
+
 module.exports = router;
