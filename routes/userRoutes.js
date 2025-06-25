f5497dd36200221a0a7c837f9d049a93ca108025
Sugumanchim <sugumanchim@gmail.com>
2025-06-25 22:34:00 +0530

    userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser } = require('../controllers/userController');
-router.post('/', registerUser);
-module.exports = router;
+const {
+  registerUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser
+} = require('../controllers/userController');
 
+router.post('/register', registerUser);
+router.get('/', getUsers);
+router.get('/:id', getUserById);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
+
+module.exports = router;
