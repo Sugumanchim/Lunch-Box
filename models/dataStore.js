e2b60b08d9c4e1fe289c80a3b685bd0b1c730857
Sugumanchim <sugumanchim@gmail.com>
2025-06-25 17:34:03 +0530

    dataStore.js

---
+++ models/dataStore.js
@@ -0,0 +1,9 @@
+module.exports = {
+  users: [],
+  mealBoxes: [
+    { id: 'm1', boxType: 'Medium', vegOrNonVeg: 'Veg', price: 100, description: 'Tasty veg meal' },
+    { id: 'm2', boxType: 'Large', vegOrNonVeg: 'NonVeg', price: 150, description: 'Chicken biryani' }
+  ],
+  orders: [],
+  subscriptions: []
+};
