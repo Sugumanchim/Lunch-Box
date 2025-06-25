ac833f5dbf0eb6b30ff303c9506ce7ea843408b9
Sugumanchim <sugumanchim@gmail.com>
2025-06-25 22:41:19 +0530

    subscriptionRoutes.js

--- routes/subscriptionRoutes.js
+++ routes/subscriptionRoutes.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { createSubscription, getSubscription } = require('../controllers/subscriptionController');
+
+const {
+  createSubscription,
+  getAllSubscriptions,
+  getSubscriptionById,
+  updateSubscription,
+  deleteSubscription
+} = require('../controllers/subscriptionController');
+
 router.post('/', createSubscription);
-router.get('/:userId', getSubscription);
+router.get('/', getAllSubscriptions);
+router.get('/:id', getSubscriptionById);
+router.put('/:id', updateSubscription);
+router.delete('/:id', deleteSubscription);
+
 module.exports = router;
