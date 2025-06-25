301689d3250e696642eb4a987da74acbc19af5d7
Sugumanchim <sugumanchim@gmail.com>
2025-06-25 22:46:38 +0530

    notifyRoutes.js

--- routes/notifyRoutes.js
+++ routes/notifyRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { notifyStatus } = require('../controllers/notifyController');
+
 router.post('/', notifyStatus);
+
 module.exports = router;
