71b96d6434370f130cbc2455b74d7346973637e2
Sugumanchim <sugumanchim@gmail.com>
2025-06-25 17:36:30 +0530

    orderRoutes.js

---
+++ routes/orderRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const { placeOrder, getOrders } = require('../controllers/orderController');
+router.post('/', placeOrder);
+router.get('/:userId', getOrders);
+module.exports = router;
