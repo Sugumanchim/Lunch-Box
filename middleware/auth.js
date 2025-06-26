7f10f139b5c2dd3acd17c21f8df448ae5b0731d8
Sugumanchim <sugumanchim@gmail.com>
2025-06-26 09:45:27 +0530

    auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
-  const token = req.headers['authorization'];
-  if (token === 'Bearer dummy-token') {
+  const token = req.headers.authorization;
+
+  if (token === 'Bearer secret123') {
     next();
   } else {
     res.status(401).json({ message: 'Unauthorized' });
