637db02de55ad3ed8ef3322a8062c9a8b2045b3d
Sugumanchim <sugumanchim@gmail.com>
2025-06-26 11:04:32 +0530

    meals.js

--- routes/meals.js
+++ routes/meals.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const {
   createMeal,
   getAllMeals,
@@ -8,11 +7,9 @@ const {
   updateMeal,
   deleteMeal
 } = require('../controllers/mealController');
-
 router.post('/', createMeal);
 router.get('/', getAllMeals);
 router.get('/:id', getMealById);
 router.put('/:id', updateMeal);
 router.delete('/:id', deleteMeal);
-
 module.exports = router;
