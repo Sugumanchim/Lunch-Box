eb41b6746c39d93e217eab4537cdca0e7b8a2eac
Sugumanchim <sugumanchim@gmail.com>
2025-06-26 11:11:23 +0530

    notifyController.js

--- controllers/notifyController.js
+++ controllers/notifyController.js
@@ -1,14 +1,11 @@
 const notifyStatus = (req, res) => {
   const { userId, message } = req.body;
-  
   if (!userId || !message) {
     return res.status(400).json({ message: 'userId and message are required' });
   }
   console.log(`Notification to User ${userId}: ${message}`);
-
   res.status(200).json({ message: 'Notification sent', userId, content: message });
 };
-
 module.exports = {
   notifyStatus
 };
