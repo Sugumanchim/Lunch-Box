c83a8ec536264a52893cec40e25392fc0ae7a32f
Sugumanchim <sugumanchim@gmail.com>
2025-06-26 11:02:54 +0530

    dataStore.js

--- models/dataStore.js
+++ models/dataStore.js
@@ -6,9 +6,6 @@ module.exports = {
     { id: 'm3', boxType: 'Small', vegOrNonVeg: 'Veg', price: 80, description: 'Light salad with rice' },
     { id: 'm4', boxType: 'Medium', vegOrNonVeg: 'NonVeg', price: 120, description: 'Egg curry meal' },
     { id: 'm5', boxType: 'Large', vegOrNonVeg: 'Veg', price: 130, description: 'Paneer butter masala combo' },
-    { id: 'm6', boxType: 'Small', vegOrNonVeg: 'NonVeg', price: 90, description: 'Chicken wrap with salad' },
-    { id: 'm7', boxType: 'Medium', vegOrNonVeg: 'Veg', price: 110, description: 'Vegetable pulao with raita' },
-    { id: 'm8', boxType: 'Large', vegOrNonVeg: 'NonVeg', price: 160, description: 'Fish curry with rice' }
   ],
   orders: [],
   subscriptions: []
