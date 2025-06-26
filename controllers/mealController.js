f81c1131950095089dd44c51afdde707d0ced376
Sugumanchim <sugumanchim@gmail.com>
2025-06-26 13:37:57 +0530

    mealController.js

--- controllers/mealController.js
+++ controllers/mealController.js
@@ -1,38 +1,29 @@
-let meals = []; 
-const createMeal = (req, res) => {
-  const { name, price, type } = req.body;
-  if (!name || !price || !type) {
-    return res.status(400).json({ message: 'Name, price, and type are required' });
+const { mealBoxes } = require('../models/dataStore');
+let mealIdCounter = 1;
+
+const addMealBox = (req, res) => {
+  const { boxType, vegOrNonVeg, price, description } = req.body;
+  if (!boxType || !vegOrNonVeg || !price || !description) {
+    return res.status(400).json({ message: 'All fields are required' });
   }
-  const newMeal = { id: Date.now(), name, price, type };
-  meals.push(newMeal);
+
+  const newMeal = {
+    id: `m${mealIdCounter++}`,
+    boxType,
+    vegOrNonVeg,
+    price,
+    description
+  };
+  mealBoxes.push(newMeal);
   res.status(201).json(newMeal);
 };
+
 const getAllMeals = (req, res) => {
-  res.json(meals);
+  res.json(mealBoxes);
 };
-const getMealById = (req, res) => {
-  const meal = meals.find(m => m.id == req.params.id);
-  if (!meal) return res.status(404).json({ message: 'Meal not found' });
-  res.json(meal);
-};
-const updateMeal = (req, res) => {
-  const meal = meals.find(m => m.id == req.params.id);
-  if (!meal) return res.status(404).json({ message: 'Meal not found' });
-  const { name, price, type } = req.body;
-  if (name) meal.name = name;
-  if (price) meal.price = price;
-  if (type) meal.type = type;
-  res.json(meal);
-};
-const deleteMeal = (req, res) => {
-  meals = meals.filter(m => m.id != req.params.id);
-  res.json({ message: 'Meal deleted' });
-};
+
 module.exports = {
-  createMeal,
-  getAllMeals,
-  getMealById,
-  updateMeal,
-  deleteMeal
+  addMealBox,
+  getAllMeals
 };
+
